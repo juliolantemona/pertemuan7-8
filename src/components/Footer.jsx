0940aa3ed22e9be6f237517b102602dc22afe0da
juliolantemona <juliolantemona7@gmail.com>
2023-04-11 19:10:32 +0700

    pertemuan7-8

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+    return <div className="py-4 text-center text-white bg-primary "> © 2023 julio_lantemona all right reserved</div>;
+};
+
+export default Footer;
